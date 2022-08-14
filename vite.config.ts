9b437a4e6ff93821042821ae675d27eaf560dbb0
mbs-code <mbscodec@gmail.com>
2022-08-15 02:23:47 +0900

    add: crxjs を導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://crxjs.dev/vite-plugin/concepts/manifest
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'hinatant',
+  version: '1.0.0',
+  permissions: ['bookmarks', 'tabs'],
+  action: {
+    default_popup: 'index.html',
+  },
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    crx({ manifest }),
+  ],
 })
